7114e7a96b88600103d680593096a8e03b232d69
mth93 <mohamedhammad89@gmail.com>
2023-12-31 06:21:22 +0200

    [FIX] import statements

--- libs/partners/google_genai/langchain_google_genai/chat_models.py
+++ libs/partners/google_genai/langchain_google_genai/chat_models.py
@@ -24,12 +24,12 @@ from urllib.parse import urlparse
 # TODO: remove ignore once the google package is published with types
 import google.generativeai as genai  # type: ignore[import]
 import requests
-from ...core.langchain_core.callbacks.manager import (
+from libs.core.langchain_core.callbacks.manager import (
     AsyncCallbackManagerForLLMRun,
     CallbackManagerForLLMRun,
 )
-from...core.langchain_core.language_models.chat_models import BaseChatModel
-from ...core.langchain_core.messages import (
+from libs.core.langchain_core.language_models.chat_models import BaseChatModel
+from libs.core.langchain_core.messages import (
     AIMessage,
     AIMessageChunk,
     BaseMessage,
@@ -39,9 +39,9 @@ from ...core.langchain_core.messages import (
     HumanMessageChunk,
     SystemMessage,
 )
-from ...core.langchain_core.outputs import ChatGeneration, ChatGenerationChunk, ChatResult
-from ...core.langchain_core.pydantic_v1 import Field, SecretStr, root_validator
-from ...core.langchain_core.utils import get_from_dict_or_env
+from libs.core.langchain_core.outputs import ChatGeneration, ChatGenerationChunk, ChatResult
+from libs.core.langchain_core.pydantic_v1 import Field, SecretStr, root_validator
+from libs.core.langchain_core.utils import get_from_dict_or_env
 from tenacity import (
     before_sleep_log,
     retry,
